dc0a00bf213f5bed236d99f51517d9a01ca98557
umesh2407 <choudharyumesh986@gmail.com>
2024-06-10 19:20:41 +0530

    removed stripe

--- backend/src/routes/OrderRoute.ts
+++ backend/src/routes/OrderRoute.ts
@@ -13,6 +13,6 @@ router.post(
   OrderController.createCheckoutSession
 );
 
-router.post("/checkout/webhook", OrderController.stripeWebhookHandler);
+// router.post("/checkout/webhook", OrderController.stripeWebhookHandler);
 
 export default router;
