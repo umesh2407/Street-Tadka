6ea1bcf91c02b225386048eca3b9a449f7c3b111
umesh2407 <choudharyumesh986@gmail.com>
2024-06-26 07:13:53 +0530

    final commit

--- frontend/src/config/order-status-config.ts
+++ frontend/src/config/order-status-config.ts
@@ -14,6 +14,6 @@ export const ORDER_STATUS: OrderStatusInfo[] = [
     progressValue: 25,
   },
   { label: "In Progress", value: "inProgress", progressValue: 50 },
-  { label: "Out for Delivery", value: "outForDelivery", progressValue: 75 },
-  { label: "Delivered", value: "delivered", progressValue: 100 },
+  { label: "Pick Up Time", value: "outForDelivery", progressValue: 75 },
+  { label: "Order picked Up", value: "delivered", progressValue: 100 },
 ];
